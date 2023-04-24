fcd125d4a8bc675e3f8f21a45773fd63cd7c73cc
gyund <gary.yund@gmail.com>
2023-04-24 18:45:26 -0400

    update more documentation

--- docs/docs/design/reference.md
+++ docs/docs/design/reference.md
@@ -16,6 +16,7 @@ Fundamental analysis, at its source, starts with having a good reliable data set
 | SEC      | [Edgar Daily Index](https://www.sec.gov/Archives/edgar/daily-index/)                         |
 | SEC      | [Edgar SEC API](https://www.sec.gov/edgar/sec-api-documentation)                             |
 | SEC      | [Financial Data Sets](https://www.sec.gov/dera/data/financial-statement-data-sets)           |
+| SEC      | [Company Tickers](https://www.sec.gov/files/company_tickers.json)                            |
 
 ## Tools
 
--- ticker/data/sec.py
+++ ticker/data/sec.py
@@ -1,16 +1,13 @@
+from zipfile import ZipFile
 from diskcache import Cache
 from datetime import date
 
 
 class ReportDate:
 
-    def __init__(self, year=date.today().year, quarter=((date.today().month-1) / 3) + 1) -> None:
-        """_summary_
-
-        Args:
-            year (int): _description_. Defaults to date.today().year.
-            quarter (int): _description_. Defaults to ((date.today().month-1) / 4)+1.
-        """
+    def __init__(self, 
+                 year : int = date.today().year, 
+                 quarter : int =((date.today().month-1) / 3) + 1):
         if year > date.today().year:
             raise ValueError(
                 "you cannot request reports in the future...that would be illegal :)")
@@ -20,13 +17,23 @@ class ReportDate:
         self.year = year
         self.quarter = quarter
 
-    def __eq__(self, other):
+    def __eq__(self, other : 'ReportDate') -> bool:
+        """
+
+        Args:
+            other ('ReportDate'): _description_
+
+        Returns:
+            bool: true if equal
+        """
         return self.quarter == other.quarter and self.year == other.year
 
 class Sec:
 
     # Format of zip example: 2023q1.zip
     _base_url = 'https://www.sec.gov/files/dera/data/financial-statement-data-sets/'
+
+    _company_tickers_url = 'https://www.sec.gov/files/company_tickers.json'
 
     def update(self, tickers: list, years: int = 5, last_report: ReportDate = ReportDate()) -> None:
         """ Update the database with information about the following stocks.
@@ -38,25 +45,44 @@ class Sec:
             years (int): number of years to go back in time. Defaults to 5.
             last_report (ReportDate): only retrieve reports from this quarter and before
         """
+        self._updateCikMappings()
+
         # Download reports for each quarter and update records for tickers specified
         download_list = self._getDownloadList(years, last_report)
         for dl in download_list:
             self._updateQuarter(dl)
             pass
 
-    def _updateQuarter(self, report_archive: str):
+    def _updateQuarter(self, report_archive: str) -> None:
         """ Update the quarter using the provided archive
 
         Args:
             report_archive (str): name of the zip file to download with the extension
         """
         # Make download request (if-needed based on file-cache)
-        # TODO: determine download agent
-
-        # Unzip request (if-needed)
+        self._downloadArchive(report_archive)
 
         # Process Quarter (if needed - based on cache)
+        self._processArchive(report_archive)
+        pass
 
+    def _updateCikMappings(self) -> None:
+        """update the CIK ticker mappings. This must be done before processing reports
+
+        The SEC stores the mappings of the CIK values to tickers in a JSON file.
+        We can download and cache this information essentially for a year. We're 
+        not interested in companies that recently listed because they don't have a 
+        long regulated record of reported earnings. When we process the records, we can 
+        ignore cik values that are not in this list.
+
+
+        Typical json for these looks like the following (without spaces or line breaks):
+
+        {"0":{"cik_str":320193,"ticker":"AAPL","title":"Apple Inc."},
+         "1":{"cik_str":789019,"ticker":"MSFT","title":"MICROSOFT CORP"},
+
+        """
+        # TODO: Make a cache request to _company_tickers_url
         pass
 
     def _getDownloadList(years: int, last_report: ReportDate) -> list[str]:
@@ -88,3 +114,32 @@ class Sec:
             else:
                 next_report.quarter -= 1
         return dl_list
+
+    def _downloadArchive(self, report_archive: str) -> None:
+        """_summary_
+
+        Args:
+            report_archive (str): name of the file w/ extension to be downloaded
+        """        
+        # TODO: determine download agent
+        pass
+
+    def _processArchive(self, report_archive: str) -> None:
+        """_summary_
+
+        Args:
+            report_archive (str): name of the file w/ extension that was downloaded
+        """   
+        arch_path = self._getArchiveStoragePath(report_archive)
+
+        with ZipFile(arch_path) as myzip:
+            # Process the mapping first
+            with myzip.open('sub.txt') as myfile:
+                # TODO: Process with pandas and filter the columns & rows
+                # print(myfile.read())
+                pass
+            with myzip.open('num.txt') as myfile:
+                # TODO: Process with pandas and filter the columns & rows
+                # print(myfile.read())
+                pass
+        
