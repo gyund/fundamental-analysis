135dd62e884ed32f80c41a9efceaeb913fa8c344
gyund <gary.yund@gmail.com>
2023-04-26 23:20:09 -0400

    update test naming

--- tests/test_sec.py
+++ tests/test_sec.py
@@ -2,37 +2,39 @@ import pytest
 import mock
 import os
 from ticker.cli import Cli
-from ticker.data.sec import Sec,ReportDate
+from ticker.data.sec import Sec, ReportDate
 from datetime import date
 from pathlib import Path
 import logging
 logger = logging.getLogger(__name__)
 
+
 def test_reportDate():
-    rd = ReportDate(2023,1)
+    rd = ReportDate(2023, 1)
     assert rd.year == date.today().year
     assert rd.quarter == 1
 
     try:
-        rd = ReportDate(date.today().year+1,1)
+        rd = ReportDate(date.today().year+1, 1)
         pytest.fail('should throw and exception')
     except ValueError as ex:
         pass
 
     try:
-        rd = ReportDate(date.today().year,0)
+        rd = ReportDate(date.today().year, 0)
         pytest.fail('should throw and exception')
     except ValueError as ex:
         pass
 
     try:
-        rd = ReportDate(date.today().year,5)
+        rd = ReportDate(date.today().year, 5)
         pytest.fail('should throw and exception')
     except ValueError as ex:
         pass
 
-class TestSecHarness:
-    sec = Sec(Path(os.path.dirname(os.path.realpath(__file__)))/ ".ticker-cache")
+
+class TestSecMockHarness:
+    sec = Sec(Path(os.path.dirname(os.path.realpath(__file__))) / ".ticker-cache")
 
     def test_update_1year(self):
         self.sec._updateCikMappings = mock.Mock()
@@ -40,7 +42,7 @@ class TestSecHarness:
         self.sec.update(['appl'], 1)
         self.sec._updateCikMappings.assert_called_once()
         assert self.sec._updateQuarter.call_count == 5
-    
+
     def test_update_2year(self):
         self.sec._updateCikMappings = mock.Mock()
         self.sec._updateQuarter = mock.Mock()
@@ -54,7 +56,7 @@ class TestSecHarness:
         self.sec.update(['appl'], 3)
         self.sec._updateCikMappings.assert_called_once()
         assert self.sec._updateQuarter.call_count == 13
-        
+
     def test_processArchive(self):
         try:
             self.sec._processArchive('bad_archive')
@@ -62,14 +64,17 @@ class TestSecHarness:
         except:
             pass
 
-@pytest.mark.skipif(os.getenv("TICKER_TEST_SEC") is None,
-                    reason="env variable TICKER_TEST_SEC not set")
-def test_integration():
+
+class TestSecHarness:
     # test using the real SEC adapter
-    sec = Sec(Path(os.path.dirname(os.path.realpath(__file__)))/ ".ticker-cache")
-    df = sec._updateCikMappings()
-    result = df[df.ticker == 'AAPL']
-    logger.debug(f'{result.ticker.iloc[0]}')
-    logger.debug(f'{result.cik_str.iloc[0]}')
-    assert result.cik_str.iloc[0] == 320193
-    assert result.ticker.iloc[0] == 'AAPL'
+    sec = Sec(Path(os.path.dirname(os.path.realpath(__file__))) / ".ticker-cache")
+
+    @pytest.mark.skipif(os.getenv("TICKER_TEST_SEC") is None,
+                        reason="env variable TICKER_TEST_SEC not set")
+    def test_updateCikMappings(self):
+        df = self.sec._updateCikMappings()
+        result = df[df.ticker == 'AAPL']
+        logger.debug(f'{result.ticker.iloc[0]}')
+        logger.debug(f'{result.cik_str.iloc[0]}')
+        assert result.cik_str.iloc[0] == 320193
+        assert result.ticker.iloc[0] == 'AAPL'
