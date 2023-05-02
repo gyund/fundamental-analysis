641042da7e5562888a8ca281f628be79fd5f640f
gyund <gary.yund@gmail.com>
2023-05-02 19:32:41 -0400

    format

--- tests/analysis/test_diluted_eps.py
+++ tests/analysis/test_diluted_eps.py
@@ -12,13 +12,21 @@ class TestCli:
     cli: Cli = Cli()
 
     def test_analyze(self):
-        self.cli.analyze(tickers=["aapl","tmo","msft"], analysis_plugin="ticker.analysis.diluted_eps")
+        self.cli.analyze(
+            tickers=["aapl", "tmo", "msft"],
+            analysis_plugin="ticker.analysis.diluted_eps",
+        )
 
     def test_export(self):
-        self.cli.export(tickers=["aapl","tmo","msft"], analysis_plugin="ticker.analysis.diluted_eps")
+        self.cli.export(
+            tickers=["aapl", "tmo", "msft"],
+            analysis_plugin="ticker.analysis.diluted_eps",
+        )
 
     def test_invalid(self):
         try:
-            self.cli.export(tickers="invalid", analysis_plugin="ticker.analysis.diluted_eps")
+            self.cli.export(
+                tickers="invalid", analysis_plugin="ticker.analysis.diluted_eps"
+            )
         except ZeroDivisionError as exc:
             pytest.fail(exc, pytrace=True)
