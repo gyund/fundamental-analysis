b813587cb8ef59b3b1b96c96afda5d9680c00e19
gyund <gary.yund@gmail.com>
2023-05-13 12:24:51 -0400

    add smoke test

--- smoke-test.sh
+++ smoke-test.sh
@@ -6,6 +6,7 @@ set -e
 
 pushd ${dir} > /dev/null
 mkdir -p ${results_dir}
+PYTHONPATH=src pipenv run python -m stocktracer analyze --tickers aapl,msft,tmo
 PYTHONPATH=src pipenv run python -m stocktracer analyze --tickers aapl,msft,tmo -a stocktracer.analysis.diluted_eps --report-file=${results_dir}/my_results.md
 
 popd > /dev/null
