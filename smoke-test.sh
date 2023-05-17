05b6ea41ad040b327e198db349b84758ccbde40b
gyund <gary.yund@gmail.com>
2023-05-16 22:23:05 -0400

    add more stocks so I can pick out differences

--- smoke-test.sh
+++ smoke-test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SMOKE_OPTIONS=${SMOKE_OPTIONS:="--refresh"}
+
 dir=`dirname $0`
 results_dir='.results'
 set -e
@@ -7,9 +9,9 @@ set -e
 pushd ${dir} > /dev/null
 mkdir -p ${results_dir}
 echo "Running default analysis..."
-PYTHONPATH=src pipenv run python -m stocktracer analyze --refresh --tickers aapl,msft,tmo > ${results_dir}/default.txt
+PYTHONPATH=src pipenv run python -m stocktracer analyze ${SMOKE_OPTIONS} --tickers aapl,msft,tmo,goog,googl,amzn,meta,acn,wm > ${results_dir}/default.txt
 
 echo "Running diluted_eps analysis..."
-PYTHONPATH=src pipenv run python -m stocktracer analyze --refresh --tickers aapl,msft,tmo -a stocktracer.analysis.diluted_eps --report-file=${results_dir}/diluted_eps.md
+PYTHONPATH=src pipenv run python -m stocktracer analyze ${SMOKE_OPTIONS} --tickers aapl,msft,tmo -a stocktracer.analysis.diluted_eps --report-file=${results_dir}/diluted_eps.md
 
 popd > /dev/null
