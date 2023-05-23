dad010eb900d466ab633f148490363620d137986
gyund <gyund@users.noreply.github.com>
2023-05-22 20:49:05 -0400

    have smoke test output csv

--- smoke-test.sh
+++ smoke-test.sh
@@ -9,9 +9,9 @@ set -e
 pushd ${dir} > /dev/null
 mkdir -p ${results_dir}
 echo "Running default analysis..."
-PYTHONPATH=src pipenv run python -m stocktracer analyze ${SMOKE_OPTIONS} --tickers aapl,msft,tmo,goog,googl,amzn,meta,acn,wm > ${results_dir}/default.txt
+PYTHONPATH=src pipenv run python -m stocktracer analyze ${SMOKE_OPTIONS} --tickers aapl,msft,tmo,goog,googl,amzn,meta,acn,wm --report-format=csv  > ${results_dir}/default.csv
 
 echo "Running diluted_eps analysis..."
-PYTHONPATH=src pipenv run python -m stocktracer analyze ${SMOKE_OPTIONS} --tickers aapl,msft,tmo -a stocktracer.analysis.diluted_eps --report-file=${results_dir}/diluted_eps.md
+PYTHONPATH=src pipenv run python -m stocktracer analyze ${SMOKE_OPTIONS} --tickers aapl,msft,tmo -a stocktracer.analysis.diluted_eps --report-format=md --report-file=${results_dir}/diluted_eps.md
 
 popd > /dev/null
