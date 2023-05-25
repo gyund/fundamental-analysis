1211e8f2d223417d26b0f0ba1f235373126b8b8b
gyund <gyund@users.noreply.github.com>
2023-05-25 19:37:59 -0400

    add smoke test for f-score

--- smoke-test.sh
+++ smoke-test.sh
@@ -8,10 +8,18 @@ set -e
 
 pushd ${dir} > /dev/null
 mkdir -p ${results_dir}
+
+base_command="python -m stocktracer analyze ${SMOKE_OPTIONS} --tickers aapl,msft,tmo,goog,googl,amzn,meta,acn,wm"
+
 echo "Running default analysis..."
-PYTHONPATH=src pipenv run python -m stocktracer analyze ${SMOKE_OPTIONS} --tickers aapl,msft,tmo,goog,googl,amzn,meta,acn,wm --report-format=csv  > ${results_dir}/default.csv
+PYTHONPATH=src pipenv run ${base_command} --report-format=csv  > ${results_dir}/default.csv
+
+echo "Running f-score analysis..."
+PYTHONPATH=src pipenv run ${base_command} --report-format=csv -a stocktracer.analysis.f_score  > ${results_dir}/fscore.csv
 
 echo "Running diluted_eps analysis..."
-PYTHONPATH=src pipenv run python -m stocktracer analyze ${SMOKE_OPTIONS} --tickers aapl,msft,tmo -a stocktracer.analysis.diluted_eps --report-format=md --report-file=${results_dir}/diluted_eps.md
+PYTHONPATH=src pipenv run ${base_command} -a stocktracer.analysis.diluted_eps --report-format=md --report-file=${results_dir}/diluted_eps.md
+
+
 
 popd > /dev/null
