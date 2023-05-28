cccf7af1d3d5d7781d54ed8b355196030339a8a3
gyund <gyund@users.noreply.github.com>
2023-05-28 12:00:13 -0400

    fix script

--- smoke-test.sh
+++ smoke-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SMOKE_OPTIONS=${SMOKE_OPTIONS:="--refresh"}
+SMOKE_OPTIONS=${SMOKE_OPTIONS:=""}
 
 dir=`dirname $0`
 results_dir='.results'
@@ -12,13 +12,13 @@ mkdir -p ${results_dir}
 base_command="python -m stocktracer analyze --tickers aapl,msft,tmo,goog,googl,amzn,meta,acn,wm"
 
 echo "Running default analysis..."
-PYTHONPATH=src pipenv run ${base_command} --report-format=csv  > ${results_dir}/default.csv
+PYTHONPATH=src pipenv run ${base_command} ${SMOKE_OPTIONS} --report-format=csv  > ${results_dir}/default.csv
 
 echo "Running f-score analysis..."
-PYTHONPATH=src pipenv run ${base_command} --report-format=csv -a stocktracer.analysis.f_score  > ${results_dir}/fscore.csv
+PYTHONPATH=src pipenv run ${base_command} ${SMOKE_OPTIONS} --report-format=csv -a stocktracer.analysis.f_score  > ${results_dir}/fscore.csv
 
 echo "Running diluted_eps analysis..."
-PYTHONPATH=src pipenv run ${base_command} -a stocktracer.analysis.diluted_eps --report-format=md --report-file=${results_dir}/diluted_eps.md
+PYTHONPATH=src pipenv run ${base_command} ${SMOKE_OPTIONS} -a stocktracer.analysis.diluted_eps --report-format=md --report-file=${results_dir}/diluted_eps.md
 
 
 
