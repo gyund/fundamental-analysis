a84c3e9f32a0949bd6ba4886385fdd0dd7a1f4cf
gyund <gyund@users.noreply.github.com>
2023-05-30 00:10:21 -0400

    work in progress, just need to get the correct key assignment

--- src/stocktracer/analysis/tensorflow.py
+++ src/stocktracer/analysis/tensorflow.py
@@ -25,7 +25,7 @@ class Analysis(AnalysisInterface):
     def analyze(self) -> Optional[pd.DataFrame]:
         # Build a training set involving good companies
         sec_filter = SecFilter(
-            tags={"EarningsPerShareDiluted"},
+            # tags={"EarningsPerShareDiluted"},
             years=5,  # Over the past 5 years
             last_report=self.options.final_report,
             only_annual=True,  # We only want the 10-K
@@ -43,11 +43,20 @@ class Analysis(AnalysisInterface):
         # Create an SEC Data Source
         table = create_normalized_sec_table(sec_filter, combined_tickers, False)
 
-        # table.calculate_return_on_assets("ROA")
-        # table.calculate_net_income("net-income")
-        # table.calculate_delta(column_name="delta-ROA", delta_of="ROA")
-        # table.calculate_debt_to_assets("debt-to-assets")
-        # table.calculate_current_ratio("current-ratio")
+        table.calculate_return_on_assets("ROA")
+        table.calculate_net_income("net-income")
+        table.calculate_delta(column_name="delta-ROA", delta_of="ROA")
+        table.calculate_debt_to_assets("debt-to-assets")
+        table.calculate_current_ratio("current-ratio")
+        table.data["good-stock"] = False
+
+        table.data.loc(axis=0)[list(tickers), "good-stock"] = True
+
+        table.data.fillna(0, inplace=True)
+
+        logging.debug(
+            f"there are {table.data['good-stock'].sum()} good stocks in the training set"
+        )
 
         # Select Training Stocks
         train_df = table.slice(ticker=list(tickers))
@@ -55,12 +64,8 @@ class Analysis(AnalysisInterface):
         # Select Assessment Stocks
         test_df = table.slice(ticker=list(tickers.difference(self.options.tickers)))
 
-        train_ds = tfdf.keras.pd_dataframe_to_tf_dataset(
-            train_df, label="EarningsPerShareDiluted"
-        )
-        test_ds = tfdf.keras.pd_dataframe_to_tf_dataset(
-            test_df, label="EarningsPerShareDiluted"
-        )
+        train_ds = tfdf.keras.pd_dataframe_to_tf_dataset(train_df, label="good-stock")
+        test_ds = tfdf.keras.pd_dataframe_to_tf_dataset(test_df, label="good-stock")
 
         # Train a Random Forest model.
         model = tfdf.keras.RandomForestModel()
